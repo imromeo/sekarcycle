82a9967056a4164845afcd63a3b05457eca64eda
imromeo <calfariae+github@gmail.com>
2025-05-03 23:13:06 +0800

    removed redundant comments

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,33 +9,11 @@
 		<ul>
 			<li>
 				<a href="/dashboard" class="flex items-center my-8">
-					<!-- <svg
-						xmlns="http://www.w3.org/2000/svg"
-						height="24px"
-						viewBox="0 -960 960 960"
-						width="24px"
-						fill="#000000"
-					>
-						<path
-							d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"
-						/>
-					</svg> -->
 					Dashboard
 				</a>
 			</li>
 			<li>
 				<a href="/collect_waste" class="flex items-center my-8">
-					<!-- <svg
-						xmlns="http://www.w3.org/2000/svg"
-						height="24px"
-						viewBox="0 -960 960 960"
-						width="24px"
-						fill="#000000"
-					>
-						<path
-							d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"
-						/>
-					</svg> -->
 					Collect Waste
 				</a>
 			</li>
